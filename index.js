541099e0c73de0852df58ff6138c3930545bf3cd
TaKeya McFadden <takeyamcfadden@gmail.com>
2024-01-11 11:38:12 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+ customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+    console.log(bestCustomer);
+}
+overwriteBestCustomer();
+
+function changeLeastFavoriteCustomer(newLeastFavorite) {
+    if (!newLeastFavorite) {
+        throw new Error('Assignment to constant variable.');
+    }
+    leastFavoriteCustomer = newLeastFavorite;
+    console.log(leastFavoriteCustomer);
+}
+changeLeastFavoriteCustomer();
